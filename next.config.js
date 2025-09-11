3087daa9fe3bdf9cfea5e44202f83f02cf9fbe44
4cozm <49065386+4cozm@users.noreply.github.com>
2025-09-11 16:44:05 +0900

    리라이트 추가 (프록시)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  async reWrites() {
+    return [{ source: "/api/:path*", destination: "http://localhost:3000/:path*" }];
+  },
+  output: "export",
   images: { unoptimized: true },
-   trailingSlash: true,
+  trailingSlash: true,
 };
 export default nextConfig;
