f21e69c43b31587780e814943245ac1ada5f8714
4cozm <17328rm@gmail.com>
2025-12-27 09:12:36 +0900

    🐛 정적 빌드로 나갈때 suspend 안된 코드 재처리

--- app/[category]/write/page.jsx
+++ app/[category]/write/page.jsx
@@ -1,5 +1,6 @@
 import { getCategoryParams } from "../../../utils/categoryPath.js";
 import PageClient from "./PageClient.jsx";
+import { Suspense } from "react";
 
 export const generateStaticParams = getCategoryParams;
 
@@ -7,5 +8,9 @@ export default async function WritePage({ params }) {
   const { category } = await params; // Next 15 비동기 처리
 
   // 클라이언트 컴포넌트로 category 전달
-  return <PageClient category={category} />;
+  return(
+    <Suspense fallback={<div className="mx-auto max-w-3xl px-4 py-8 text-white/70">불러오는 중...</div>}>
+      <PageClient category={category} />;
+    </Suspense>);
+
 }
